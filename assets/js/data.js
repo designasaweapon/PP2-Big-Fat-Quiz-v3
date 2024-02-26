56bd9c6b84ae7d9c6e4c177283c5ec68f9ba58ab
designasaweapon <designasaweapon@gmail.com>
2024-02-26 14:44:37 +0000

    quiz question data js added

---
+++ assets/js/data.js
@@ -0,0 +1,105 @@
+// various question & answer arrays
+
+const questions = [
+    {
+        question: 'What was the name of Prince Harry's memoir published in January?',
+        answer: [
+            {text: 'Hair', correct: false },
+            {text: 'Spare', correct: true},
+            {text: 'Her', correct: false},
+            {text: 'Heir!', correct: false}
+        ],
+        fact: "Harry became a working member of the Royal Family officially at 21.",
+    },
+    {
+        question: 'At the beginning of the year which country re-opened its borders to international visitors post-Covid?',
+        answer: [
+            {text: 'Poland', correct: false },
+            {text: 'Ireland', correct: false},
+            {text: 'China', correct: true},
+            {text: 'USA', correct: false}
+        ], 
+        fact: "China announced further relaxations of its COVID-19 controls and the reopening of its borders in 2023."
+    },
+    {
+        question: 'What did Kylie Jenner wear to the Schiaparelli fashion show in January?',
+        answer: [
+            {text: 'lion head dress', correct: true },
+            {text: 'goat head coat', correct: false},
+            {text: 'Ant head pants', correct: false},
+            {text: 'Jackal skin jacket', correct: false}
+        ],
+        fact: "The realistic lion's head was actually hand-sculpted out of foam, hand-embroidered with wool and silk faux fur."
+    },
+    {
+        question: 'What is the name of the fungal infection that caused a global pandemic in the TV series The Last of Us?',
+        answer: [
+            {text: 'Gordonfecs', correct: false},
+            {text: 'Geordishors', correct: false},
+            {text: 'Cordyceps', correct: true},
+            {text: 'Cordongons', correct: false}
+        ], 
+        fact: "The actual fungus – cordyceps – is quite real. It can in fact hijack the minds of ants."
+    },
+    {
+        question: "Who won the Best Actress award at the 2023 Oscars?",
+        answer: [
+            {text: 'Michelle Williams', correct: false },
+            {text: 'Michelle Yeoh', correct: true},
+            {text: 'Angela Bassett', correct: false},
+            {text: 'Beyonce Knowles', correct: false}
+        ],
+        fact: "She won the award for best performance in a leading role for 'Everything Everywhere All at Once'"
+    },
+    {
+        question: "What date did Taylor Swift's Eras Tour commence?",
+        answer: [
+            {text: 'July 4th 2023', correct: false },
+            {text: 'April 1st 2023', correct: false},
+            {text: 'March 17th 2023', correct: true},
+            {text: 'December 25th 2023', correct: false}
+        ],
+        fact: "The Eras Tour became the first concert tour to surpass $1 billion in revenue and the highest-grossing ever."
+    },
+    {
+        question: "In April which country became the 31st country to join NATO?",
+        answer: [
+            {text: 'Finland', correct: true },
+            {text: 'Ireland', correct: false},
+            {text: 'Iceland', correct: false},
+            {text: 'Holland', correct: false}
+        ],
+        fact: "53 percent of the population were in favour of NATO membership during a referendum."
+    },
+    {
+        question: "Who won the Women’s Six Nations?",
+        answer: [
+            {text: 'Wales', correct: false },
+            {text: 'Scotland', correct: false},
+            {text: 'Ireland', correct: false},
+            {text: 'England', correct: true}
+        ],
+        fact: "Between 1996 and 2023, the country with the most Championship titles in the Women's Six Nations was England, having won 20"
+    },
+    {
+        question: "Where was King Charles' coronation held?",
+        answer: [
+            {text: 'Trafalgar Square', correct: false},
+            {text: 'Covent Garden', correct: false},
+            {text: 'Westminster Abbey', correct: true},
+            {text: '10 Downing Street', correct: false}
+        ],
+        fact: "Charles himself was born at Buckingham Palace on 14 November 1948, by caesarean section, after a 30-hour labour.",
+    },
+    {
+        question: "What is the name of the Barbie played by Kate McKinnon in the Barbie movie?",
+        answer: [
+            {text: 'Weird Barbie', correct: true},
+            {text: 'Overacting Barbie', correct: false},
+            {text: 'Never Funny Barbie', correct: false},
+            {text: 'Meh Barbie', correct: false}
+        ],
+        fact: "The Humanity Montage Featured Real Clips of Cast and Crew's Family."
+    },
+
+];
