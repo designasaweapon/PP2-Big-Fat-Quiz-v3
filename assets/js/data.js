66b5bd765544ebe67010057c0730c64938dc3dae
designasaweapon <designasaweapon@gmail.com>
2024-02-27 23:41:19 +0000

    Update data.js - Content typos & copy amended

--- assets/js/data.js
+++ assets/js/data.js
@@ -4,17 +4,17 @@ const questions = [
     {
         question: 'Name of Prince Harrys memoir published in January?',
         answer: [
-            {text: 'Hair', correct: false },
+            {text: 'Hair', correct: false},
             {text: 'Spare', correct: true},
             {text: 'Her', correct: false},
             {text: 'Heir!', correct: false}
         ],
-        fact: "Harry became a working member of the Royal Family officially at 21.",
+        fact: "Harry became an official working member of the Royal Family at just 21."
     },
     {
         question: 'Which country only re-opened its borders early-2023 to international visitors post-Covid?',
         answer: [
-            {text: 'Poland', correct: false },
+            {text: 'Poland', correct: false},
             {text: 'Ireland', correct: false},
             {text: 'China', correct: true},
             {text: 'USA', correct: false}
@@ -24,47 +24,47 @@ const questions = [
     {
         question: 'What did Kylie Jenner wear to the Schiaparelli fashion show in January?',
         answer: [
-            {text: 'lion head dress', correct: true },
-            {text: 'goat head coat', correct: false},
-            {text: 'Ant head pants', correct: false},
-            {text: 'Jackal skin jacket', correct: false}
+            {text: 'Lion head dress', correct: true},
+            {text: 'Goat head coat', correct: false},
+            {text: 'Ants head pants', correct: false},
+            {text: 'Jackal head jacket', correct: false}
         ],
         fact: "The realistic lion's head was actually hand-sculpted out of foam, hand-embroidered with wool and silk faux fur."
     },
     {
         question: 'What is the name of the fungal infection that caused a global pandemic in the TV series The Last of Us?',
         answer: [
-            {text: 'Gordonfecs', correct: false},
+            {text: 'Gordonfucs', correct: false},
             {text: 'Geordishors', correct: false},
             {text: 'Cordyceps', correct: true},
             {text: 'Cordongons', correct: false}
         ], 
-        fact: "The actual fungus – cordyceps – is quite real. It can in fact hijack the minds of ants."
+        fact: "The fungus – cordyceps – is actually quite real and it can in fact hijack the minds of ants."
     },
     {
         question: "Who won the Best Actress award at the 2023 Oscars?",
         answer: [
-            {text: 'Michelle Williams', correct: false },
+            {text: 'Michelle Williams', correct: false},
             {text: 'Michelle Yeoh', correct: true},
             {text: 'Angela Bassett', correct: false},
             {text: 'Beyonce Knowles', correct: false}
         ],
-        fact: "She won the award for best performance in a leading role for 'Everything Everywhere All at Once'"
+        fact: "Michelle won the award for best performance in a leading role in 'Everything Everywhere All at Once'."
     },
     {
-        question: "What date did Taylor Swift's Eras Tour commence?",
+        question: "What date did Taylor Swift's record-breaking Eras Tour commence?",
         answer: [
-            {text: 'July 4th 2023', correct: false },
+            {text: 'July 4th 2023', correct: false},
             {text: 'April 1st 2023', correct: false},
             {text: 'March 17th 2023', correct: true},
             {text: 'December 25th 2023', correct: false}
         ],
-        fact: "The Eras Tour became the first concert tour to surpass $1 billion in revenue and the highest-grossing ever."
+        fact: "In 2023 the Eras Tour became the first concert tour to surpass $1Billion in revenue and is the highest-grossing tour ever."
     },
     {
-        question: "In April which country became the 31st country to join NATO?",
+        question: "In April 2023 which country became the 31st country to join NATO?",
         answer: [
-            {text: 'Finland', correct: true },
+            {text: 'Finland', correct: true},
             {text: 'Ireland', correct: false},
             {text: 'Iceland', correct: false},
             {text: 'Holland', correct: false}
@@ -72,9 +72,9 @@ const questions = [
         fact: "53 percent of the population were in favour of NATO membership during a referendum."
     },
     {
-        question: "Who won the Women’s Six Nations?",
+        question: "Who won the 2023 Women’s Six Nations Rugby Championship?",
         answer: [
-            {text: 'Wales', correct: false },
+            {text: 'Wales', correct: false},
             {text: 'Scotland', correct: false},
             {text: 'Ireland', correct: false},
             {text: 'England', correct: true}
@@ -82,7 +82,7 @@ const questions = [
         fact: "Between 1996 and 2023, the country with the most Championship titles in the Women's Six Nations was England, having won 20"
     },
     {
-        question: "Where was King Charles' coronation held?",
+        question: "Where was King Charles' coronation ceremony held?",
         answer: [
             {text: 'Trafalgar Square', correct: false},
             {text: 'Covent Garden', correct: false},
@@ -92,14 +92,14 @@ const questions = [
         fact: "Charles himself was born at Buckingham Palace on 14 November 1948, by caesarean section, after a 30-hour labour.",
     },
     {
-        question: "What is the name of the Barbie played by Kate McKinnon in the Barbie movie?",
+        question: "What is the name of the Barbie played by Kate McKinnon in the 2023 'Barbie' movie?",
         answer: [
             {text: 'Weird Barbie', correct: true},
             {text: 'Overacting Barbie', correct: false},
             {text: 'Never Funny Barbie', correct: false},
             {text: 'Meh Barbie', correct: false}
         ],
-        fact: "The Humanity Montage Featured Real Clips of Cast and Crew's Family."
+        fact: "The Humanity montage featured real clips of the Barbie cast and crew's actual family members."
     },
 
 ];
